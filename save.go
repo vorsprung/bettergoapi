62caf240ea4da129c8edfb155aee82501274a500
james andrews <jandrews@commoninf.com>
2025-04-02 11:55:03 +0100

    merge fix

--- save.go
+++ save.go
@@ -2,28 +2,28 @@ package bettergoapi
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"io"
 	"os"
 
-	"github.com/aws/aws-sdk-go/aws"
-
-	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 type MyUploader interface {
-	Upload(*s3manager.UploadInput) (*s3manager.UploadOutput, error)
+	Upload(ctx context.Context, input *s3.PutObjectInput, opts ...func(*manager.Uploader)) (*manager.UploadOutput, error)
 }
 
 type MyDownloader interface {
-	Download(io.WriterAt, *s3.GetObjectInput, ...func(*s3manager.Downloader)) (int64, error)
+	Download(ctx context.Context, w io.WriterAt, input *s3.GetObjectInput, opts ...func(*manager.Downloader)) (n int64, err error)
 }
 
-// call as SaveToS3(monitor, "monitors.json", s3manager.NewUploader(getAWSsession()))
-func SaveToS3(monitor []Monitor, path string, uploader MyUploader) error {
+// call as SaveToS3(ctx, monitor, "monitors.json", manager.NewUploader(client))
+func SaveToS3(ctx context.Context, monitor []Monitor, path string, uploader MyUploader) error {
 	data, _ := json.MarshalIndent(monitor, "  ", " ")
-	_, err := uploader.Upload(&s3manager.UploadInput{
+	_, err := uploader.Upload(ctx, &s3.PutObjectInput{
 		Bucket: aws.String("bettergoapi-monitor"),
 		Key:    aws.String(path),
 		Body:   bytes.NewReader(data),
@@ -31,11 +31,11 @@ func SaveToS3(monitor []Monitor, path string, uploader MyUploader) error {
 	return err
 }
 
-// call as LoadFromS3("monitors.json", s3manager.NewDownloader(getAWSsession()))
-func LoadFromS3(path string, downloader MyDownloader) ([]Monitor, error) {
+// call as LoadFromS3(ctx, "monitors.json", manager.NewDownloader(client))
+func LoadFromS3(ctx context.Context, path string, downloader MyDownloader) ([]Monitor, error) {
 	var monitor []Monitor
-	buf := aws.NewWriteAtBuffer([]byte{})
-	_, err := downloader.Download(buf, &s3.GetObjectInput{
+	buf := manager.NewWriteAtBuffer([]byte{})
+	_, err := downloader.Download(ctx, buf, &s3.GetObjectInput{
 		Bucket: aws.String("bettergoapi-monitor"),
 		Key:    aws.String(path),
 	})
